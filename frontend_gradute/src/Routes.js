e9d0e934ba498f66c415febeb46db47e4b217836
thi99 <nguyendinhthi23041999@gmail.com>
2022-04-07 21:23:22 +0700

    update UI Dashbord

--- frontend_gradute/src/Routes.js
+++ frontend_gradute/src/Routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminLayout from "./layouts/AdminLayout";
 import AddCategoryFood from "./pages/admin/categoryFoods/Add";
 import ListCategoryFoods from "./pages/admin/categoryFoods/list";
+import Dashboard from "./pages/admin/Dashboard";
 
 export default function Routess(props) {
     return (
@@ -11,6 +12,7 @@ export default function Routess(props) {
                     <Route exact path="/admin/categoryFoods" element={<ListCategoryFoods {...props} />}/>
                     <Route exact path="/admin/categoryFoodAdd" element={<AddCategoryFood {...props} />} />
                     <Route exact path="/admin/CategoryFoodEdit/:id" element={<AddCategoryFood {...props} />} />
+                    <Route exact path="/admin" element={<Dashboard {...props} />} />
                 </Routes>
             </AdminLayout>
         </BrowserRouter>
